1083adc6ec59f1c0d15e57826335f1580b5509f3
varun sai <varun@idealabs.fyi>
2025-03-05 16:14:17 +0530

    Update audioink.py

--- audioink.py
+++ audioink.py
@@ -170,22 +170,25 @@ def convert_to_wav(audio_file):
 
 def main():
 
-    # Add Water Wave Animation Instead of Mic Image
-    st.markdown('''
-    <div class="water-round-container">
-        <div class="water-wave1"></div>
-        <div class="water-wave2"></div>
-        <div class="water-wave3"></div>
-    </div>
-    ''', unsafe_allow_html=True)
-    
-    # Hero Title
-    st.markdown('''
-    <h1 class="hero-title">
-        Say it loud <span class="highlight">let words flow,</span><br>
-        Upload with ease <span class="highlight">watch text grow.</span>
-    </h1>
-    ''', unsafe_allow_html=True)
+    # Add Water Wave Animation 
+    col1, col2 = st.columns([1, 3])
+    with col1:
+        # Add Water Wave Animation Instead of Mic Image
+        st.markdown('''
+        <div class="water-round-container">
+            <div class="water-wave1"></div>
+            <div class="water-wave2"></div>
+            <div class="water-wave3"></div>
+        </div>
+        ''', unsafe_allow_html=True)
+    with col2:
+        # Hero Title
+        st.markdown('''
+        <h1 class="hero-title">
+            Say it loud <span class="highlight">let words flow,</span><br>
+            Upload with ease <span class="highlight">watch text grow.</span>
+        </h1>
+        ''', unsafe_allow_html=True)
     
     # Subtitle
     st.markdown('''
