5107f05569803e486b71860d50a21ad36a54e3a9
varun sai <varun@idealabs.fyi>
2025-03-06 11:29:42 +0530

    Update audioink.py

--- audioink.py
+++ audioink.py
@@ -242,6 +242,8 @@ def main():
     ''', unsafe_allow_html=True)
     
     # Custom Toggle Buttons
+    st.markdown("<p>Choose Input Method</p>", unsafe_allow_html=True)
+    
     col1, col2 = st.columns(2)
     
     with col1:
